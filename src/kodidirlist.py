2ab0811542fae48ef6ba12d8b72711a10d935e50
test <test@test.com>
2022-04-09 20:24:38 +0100

    st

--- src/kodidirlist.py
+++ src/kodidirlist.py
@@ -1,18 +1,17 @@
-#!/usr/bin/python
-
+#!python
 import os
 
 def print_sizes(root):
     for name in sorted(os.listdir(root)):
         if name == 'index.html':
             continue
-        print '<tr><td><a href="%s">%s</a></td><td>%dB</td></tr>' % (name, name, os.stat(name).st_size)
+        print('<tr><td><a href="%s">%s</a></td><td>%dB</td></tr>' % (name, name, os.stat(name).st_size))
 
 
-print '<html>'
-print '<body>'
-print '<table>'
+print( '<html>')
+print( '<body>')
+print( '<table>')
 print_sizes('.')
-print '</table>'
-print '</body>'
-print '</html>'
+print( '</table>')
+print( '</body>')
+print( '</html>')
